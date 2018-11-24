eaabb062ae53c70469750acf1bef42760a2cfdd1
kalelman <erick_rojas_perez@hotmail.com>
2018-11-23 23:26:43 -0500

    Implementing Toolbar in the project

--- app/src/main/java/com/kalelman/singleton_pattern_android/MainActivity.java
+++ app/src/main/java/com/kalelman/singleton_pattern_android/MainActivity.java
@@ -2,12 +2,38 @@ package com.kalelman.singleton_pattern_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setTitle(R.string.toolbar_title);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return true;
     }
 }
