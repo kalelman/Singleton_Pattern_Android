64e8b0f944a1191ae154573b00aac71c73e218fe
kalelman <erick_rojas_perez@hotmail.com>
2018-11-26 12:29:27 -0500

    Implementation of Navigation Drawer in the project

--- app/src/main/java/com/kalelman/singleton_pattern_android/MainActivity.java
+++ app/src/main/java/com/kalelman/singleton_pattern_android/MainActivity.java
@@ -1,10 +1,17 @@
 package com.kalelman.singleton_pattern_android;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
 import butterknife.BindView;
@@ -14,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.drawer)
+    DrawerLayout drawerLayout;
+    @BindView(R.id.navigation_view)
+    NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,45 @@ public class MainActivity extends AppCompatActivity {
             getSupportActionBar().setTitle(R.string.toolbar_title);
         }
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.openDrawer, R.string.closeDrawer) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+
+                switch (menuItem.getItemId()) {
+                    case R.id.comedy:
+                        Toast.makeText(MainActivity.this, "Comedy", Toast.LENGTH_SHORT).show();
+                        return true;
+
+                    case R.id.drama:
+                        Toast.makeText(MainActivity.this, "Drama", Toast.LENGTH_SHORT).show();
+                        return true;
+
+                    case R.id.mistery:
+                        Toast.makeText(MainActivity.this, "Mistery", Toast.LENGTH_SHORT).show();
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
     }
 
     @Override
